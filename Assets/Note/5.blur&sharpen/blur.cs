44da6bbeb3eed23eac4baf856154ce0ad133f00f
Setsuodu <339131001@qq.com>
2018-02-06 15:01:42 +0800

    sharpen

--- Assets/Note/5.blur&sharpen/blur.cs
+++ Assets/Note/5.blur&sharpen/blur.cs
@@ -10,10 +10,12 @@ public class blur : MonoBehaviour
     [SerializeField] private Image m_blurImage;
     [SerializeField] private Toggle m_origToggle;
     [SerializeField] private Toggle m_saltToggle;
-    [SerializeField] private Toggle m_simpleToggle;
+    [SerializeField] private Toggle m_simpleBlurToggle;
     [SerializeField] private Toggle m_gaussianToggle;
     [SerializeField] private Toggle m_medianToggle;
     [SerializeField] private Toggle m_bilateralToggle;
+    [SerializeField] private Toggle m_simpleSharpenToggle;
+    [SerializeField] private Toggle m_sobelToggle;
     Mat srcMat;
     byte[] byteArray;
 
@@ -21,10 +23,12 @@ public class blur : MonoBehaviour
     {
         m_origToggle.onValueChanged.AddListener((bool value) => OnOrig(value));
         m_saltToggle.onValueChanged.AddListener((bool value) => OnSalt(value));
-        m_simpleToggle.onValueChanged.AddListener((bool value) => OnSimpleBlur(value));
+        m_simpleBlurToggle.onValueChanged.AddListener((bool value) => OnSimpleBlur(value));
         m_gaussianToggle.onValueChanged.AddListener((bool value) => OnGaussianBlur(value));
         m_medianToggle.onValueChanged.AddListener((bool value) => OnMedianBlur(value));
         m_bilateralToggle.onValueChanged.AddListener((bool value) => OnBilateralFilter(value));
+        m_simpleSharpenToggle.onValueChanged.AddListener((bool value) => OnSimpleSharpen(value));
+        m_sobelToggle.onValueChanged.AddListener((bool value) => OnSobelSharpen(value));
     }
 
     void Start()
@@ -78,6 +82,8 @@ public class blur : MonoBehaviour
         m_blurImage.preserveAspect = true;
         Utils.matToTexture2D(dstMat, t2d);
     }
+
+    #region Blur
 
     //简单模糊
     void OnSimpleBlur(bool value)
@@ -158,4 +164,58 @@ public class blur : MonoBehaviour
         m_blurImage.preserveAspect = true;
         Utils.matToTexture2D(dstMat, t2d);
     }
+
+    #endregion
+
+    #region Sharpen
+
+    //简单锐化
+    void OnSimpleSharpen(bool value)
+    {
+        m_blurImage.enabled = true;
+        if (!value) return;
+
+        Mat dstMat = new Mat();
+        Mat kernel = new Mat(3, 3, CvType.CV_32F, new Scalar(-1));
+        kernel.put(0, 0, 0, -1, 0, -1, 5, -1, 0, -1, 0);
+        //对图像srcMat和自定义核kernel做卷积，输出到dstMat
+        Imgproc.filter2D(srcMat, dstMat, srcMat.depth(), kernel);
+
+        Texture2D t2d = new Texture2D(dstMat.width(), dstMat.height());
+        Sprite sp = Sprite.Create(t2d, new UnityEngine.Rect(0, 0, t2d.width, t2d.height), Vector2.zero);
+        m_blurImage.sprite = sp;
+        m_blurImage.preserveAspect = true;
+        Utils.matToTexture2D(dstMat, t2d);
+    }
+
+    //索贝尔算子锐化
+    void OnSobelSharpen(bool value)
+    {
+        m_blurImage.enabled = true;
+        if (!value) return;
+
+        Mat dstMat = new Mat();
+        Mat dst_x = new Mat();
+        Mat abs_dst_x = new Mat();
+        Mat dst_y = new Mat();
+        Mat abs_dst_y = new Mat();
+
+        //水平方向梯度
+        Imgproc.Sobel(srcMat, dst_x, srcMat.depth(), 1, 0, 3, 1, 0);
+        //垂直方向梯度
+        Imgproc.Sobel(srcMat, dst_y, srcMat.depth(), 0, 1, 3, 1, 0);
+
+        Core.convertScaleAbs(dst_x, abs_dst_x); //先缩放元素再取绝对值
+        Core.convertScaleAbs(dst_y, abs_dst_y);
+        Core.addWeighted(abs_dst_x, 0.5d, abs_dst_y, 0.5d, 0, dstMat); //x,y方向梯度，平均叠加融合
+        Core.addWeighted(dstMat, 0.5d, srcMat, 1d, 0, dstMat); //把边缘叠加到原图
+
+        Texture2D t2d = new Texture2D(dstMat.width(), dstMat.height());
+        Sprite sp = Sprite.Create(t2d, new UnityEngine.Rect(0, 0, t2d.width, t2d.height), Vector2.zero);
+        m_blurImage.sprite = sp;
+        m_blurImage.preserveAspect = true;
+        Utils.matToTexture2D(dstMat, t2d);
+    }
+
+    #endregion
 }
