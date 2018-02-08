2334b361f9c9cdd5aa2942bb2bd3f63f6bf30961
Setsuodu <339131001@qq.com>
2018-02-08 18:43:46 +0800

    face image detect

--- Assets/Note/face/FaceDetect.cs
+++ Assets/Note/face/FaceDetect.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 using OpenCVForUnity;
 using OpenCVForUnityExample;
+using DlibFaceLandmarkDetector;
 
 public class FaceDetect : MonoBehaviour
 {
-    [SerializeField] private Image m_dstImage;
+    [SerializeField] private Image m_srcImage;
+    [SerializeField] private Image m_webcamImage;
     [SerializeField]  Texture2D texture;
     Color32[] colors;
     Mat rgbaMat;
+    string sp_human_face_68_dat_filepath;
 
     WebCamTextureToMatHelper webCamTextureToMatHelper;
 
@@ -18,6 +21,8 @@ public class FaceDetect : MonoBehaviour
     {
         webCamTextureToMatHelper = gameObject.GetComponent<WebCamTextureToMatHelper>();
         webCamTextureToMatHelper.Initialize();
+
+        Run();
     }
 
     void Update()
@@ -25,10 +30,9 @@ public class FaceDetect : MonoBehaviour
         if (webCamTextureToMatHelper.IsPlaying() && webCamTextureToMatHelper.DidUpdateThisFrame())
         {
             rgbaMat = webCamTextureToMatHelper.GetMat();
-            Utils.matToTexture2D(rgbaMat, texture, webCamTextureToMatHelper.GetBufferColors());
+            OpenCVForUnity.Utils.matToTexture2D(rgbaMat, texture, webCamTextureToMatHelper.GetBufferColors());
         }
     }
-
 
     public void OnWebCamTextureToMatHelperInitialized()
     {
@@ -39,8 +43,8 @@ public class FaceDetect : MonoBehaviour
 
         texture = new Texture2D(webCamTextureMat.cols(), webCamTextureMat.rows(), TextureFormat.RGBA32, false);
         Sprite sp = Sprite.Create(texture, new UnityEngine.Rect(0, 0, texture.width, texture.height), Vector2.zero);
-        m_dstImage.sprite = sp;
-        m_dstImage.preserveAspect = true;
+        m_webcamImage.sprite = sp;
+        m_webcamImage.preserveAspect = true;
     }
 
     public void OnWebCamTextureToMatHelperDisposed()
@@ -53,5 +57,46 @@ public class FaceDetect : MonoBehaviour
     public void OnWebCamTextureToMatHelperErrorOccurred(WebCamTextureToMatHelper.ErrorCode errorCode)
     {
         Debug.Log("On Error Occurred " + errorCode);
+    }
+
+    void Run()
+    {
+        sp_human_face_68_dat_filepath = DlibFaceLandmarkDetector.Utils.getFilePath("sp_human_face_68.dat");
+        FaceLandmarkDetector faceLandmarkDetector = new FaceLandmarkDetector(sp_human_face_68_dat_filepath);
+
+        Mat imgMat = Imgcodecs.imread(Application.dataPath + "/Textures/face.jpg");
+        Imgproc.cvtColor(imgMat, imgMat, Imgproc.COLOR_BGR2RGB);
+        OpenCVForUnityUtils.SetImage(faceLandmarkDetector, imgMat);
+
+        //detect face rectdetecton
+        List<FaceLandmarkDetector.RectDetection> detectResult = faceLandmarkDetector.DetectRectDetection();
+
+        foreach (var result in detectResult)
+        {
+            Debug.Log("rect : " + result.rect);
+            Debug.Log("detection_confidence : " + result.detection_confidence);
+            Debug.Log("weight_index : " + result.weight_index);
+            //Debug.Log ("face : " + rect);
+
+            Imgproc.putText(imgMat, "" + result.detection_confidence, new Point(result.rect.xMin, result.rect.yMin - 20), Core.FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(255, 255, 255, 255), 1, Imgproc.LINE_AA, false);
+            Imgproc.putText(imgMat, "" + result.weight_index, new Point(result.rect.xMin, result.rect.yMin - 5), Core.FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(255, 255, 255, 255), 1, Imgproc.LINE_AA, false);
+
+            //detect landmark points
+            List<Vector2> points = faceLandmarkDetector.DetectLandmark(result.rect);
+
+            Debug.Log("face points count : " + points.Count);
+            //draw landmark points
+            OpenCVForUnityUtils.DrawFaceLandmark(imgMat, points, new Scalar(0, 255, 0, 255), 2);
+
+            //draw face rect
+            OpenCVForUnityUtils.DrawFaceRect(imgMat, result.rect, new Scalar(255, 0, 0, 255), 2);
+        }
+        faceLandmarkDetector.Dispose();
+
+        Texture2D t2d = new Texture2D(imgMat.cols(), imgMat.rows(), TextureFormat.RGBA32, false);
+        OpenCVForUnity.Utils.matToTexture2D(imgMat, t2d);
+        Sprite sp = Sprite.Create(t2d, new UnityEngine.Rect(0, 0, t2d.width, t2d.height), Vector2.zero);
+        m_srcImage.sprite = sp;
+        m_srcImage.preserveAspect = true;
     }
 }
