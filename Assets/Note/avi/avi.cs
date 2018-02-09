84c549ad58b35e15034ff74b7f4c34a88137cb40
Setsuodu <339131001@qq.com>
2018-02-09 19:52:14 +0800

    2017.3.1
    update unity to 2017.3.1

--- Assets/Note/avi/avi.cs
+++ Assets/Note/avi/avi.cs
@@ -4,40 +4,43 @@ using UnityEngine;
 using UnityEngine.UI;
 using OpenCVForUnity;
 
-public class avi : MonoBehaviour
+namespace OpenCVForUnityExample
 {
-    [SerializeField] private Image m_showImage;
-    private Texture2D t2d;
-    Mat rgbMat = new Mat();
-    Color32[] colors;
-    VideoCapture capture;
-
-    void Start()
+    public class avi : MonoBehaviour
     {
-        capture = new VideoCapture();
-        capture.open(Utils.getFilePath("dance.avi"));
-        if (capture.isOpened())
+        [SerializeField] private Image m_showImage;
+        private Texture2D t2d;
+        Mat rgbMat = new Mat();
+        Color32[] colors;
+        VideoCapture capture;
+
+        void Start()
         {
-            Debug.Log("opened");
+            capture = new VideoCapture();
+            capture.open(Utils.getFilePath("dance.avi"));
+            if (capture.isOpened())
+            {
+                Debug.Log("opened");
+            }
+
+            capture.grab(); //抓取
+            capture.retrieve(rgbMat, 0); //检索
+
+            colors = new Color32[rgbMat.width() * rgbMat.height()];
+            t2d = new Texture2D(rgbMat.width(), rgbMat.height());
+            Sprite sp = Sprite.Create(t2d, new UnityEngine.Rect(0, 0, t2d.width, t2d.height), Vector2.zero);
+            m_showImage.sprite = sp;
+            m_showImage.preserveAspect = true;
         }
 
-        capture.grab(); //抓取
-        capture.retrieve(rgbMat, 0); //检索
-
-        colors = new Color32[rgbMat.width() * rgbMat.height()];
-        t2d = new Texture2D(rgbMat.width(), rgbMat.height());
-        Sprite sp = Sprite.Create(t2d, new UnityEngine.Rect(0, 0, t2d.width, t2d.height), Vector2.zero);
-        m_showImage.sprite = sp;
-        m_showImage.preserveAspect = true;
-    }
-
-    void Update()
-    {
-        if(capture.grab())
+        void Update()
         {
-            capture.retrieve(rgbMat, 0);
-            Imgproc.cvtColor(rgbMat, rgbMat, Imgproc.COLOR_BGR2RGB); //BGR转RGB
-            Utils.matToTexture2D(rgbMat, t2d, colors);
+            if (capture.grab())
+            {
+                capture.retrieve(rgbMat, 0);
+                Imgproc.cvtColor(rgbMat, rgbMat, Imgproc.COLOR_BGR2RGB); //BGR转RGB
+                Utils.matToTexture2D(rgbMat, t2d, colors);
+            }
         }
     }
 }
